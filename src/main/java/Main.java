bbcb0be8b687b9de9c18aae7cc16a4419b666a30
Andrii Kalie <andrejkale@gmail.com>
2022-12-11 21:39:29 +0200

    Tasks 4 - 7 were added gitignore was updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
@@ -7,74 +7,88 @@ public class Main {
         /*Ввести n чисел с консоли.
         1. Найти самое короткое и самое длинное число. Вывести найденные числа
         и их длину*/
-        System.out.println("Enter a number \"n\" of digitals");
+        System.out.println("Enter a number \"n\" of digits");
         Scanner scanner = new Scanner(System.in);
         String digitalLong;
         String digitalShort;
 
 
         int n = scanner.nextInt();
-        String[] digitals = new String[n];
+        String[] digits = new String[n];
 
-        for (int i = 0; i < digitals.length; i++) {
-            System.out.println("Enter a digitals");
-            digitals[i] = scanner.next();
+        for (int i = 0; i < digits.length; i++) {
+            System.out.println("Enter a digits");
+            digits[i] = scanner.next();
         }
 
-        int min = digitals[0].length();
-        int max = digitals[0].length();
+        int min = digits[0].length();
+        int max = digits[0].length();
         int shortIndex = 0;
         int longIndex = 0;
 
-        for (int i = 0; i < digitals.length; i++) {
-            if (digitals[i].length() < min) {
-                min = digitals[i].length();
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i].length() < min) {
+                min = digits[i].length();
                 shortIndex = i;
             }
         }
 
-        for (int i = 0; i < digitals.length; i++) {
-            if (digitals[i].length() > max) {
-                max = digitals[i].length();
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i].length() > max) {
+                max = digits[i].length();
                 longIndex = i;
             }
         }
 
-        digitalShort = digitals[shortIndex];
-        digitalLong = digitals[longIndex];
+        digitalShort = digits[shortIndex];
+        digitalLong = digits[longIndex];
 
         System.out.println("Short digital = " + digitalShort + " and length = " + min);
         System.out.println("Long digital = " + digitalLong  + " and length = " + max);
 
         /* 2. Упорядочить и вывести числа в порядке возрастания (убывания) значений
         их длины.*/
-        for(int i = 1; i < digitals.length; i++) { // sort by desc
-            String temp = digitals[i];
+        for(int i = 1; i < digits.length; i++) { // sort by desc
+            String temp = digits[i];
             int j;
-            for(j = i - 1; j >= 0 && digitals[j].length() > temp.length(); j--) {
-                digitals[j + 1] = digitals[j];
+            for(j = i - 1; j >= 0 && digits[j].length() > temp.length(); j--) {
+                digits[j + 1] = digits[j];
             }
-            digitals[j + 1] = temp;
+            digits[j + 1] = temp;
         }
-        System.out.println("Array string sorted by Ascending order " + Arrays.toString(digitals));
+        System.out.println("Array string sorted by Ascending order " + Arrays.toString(digits));
 
-        for(int i = 1; i < digitals.length; i++) { // sort by asc
-            String temp = digitals[i];
+        for(int i = 1; i < digits.length; i++) { // sort by asc
+            String temp = digits[i];
             int j;
-            for(j= i - 1; j >= 0 && digitals[j].length() < temp.length(); j--) {
-                digitals[j + 1] = digitals[j];
+            for(j= i - 1; j >= 0 && digits[j].length() < temp.length(); j--) {
+                digits[j + 1] = digits[j];
             }
-            digitals [j + 1] = temp;
+            digits [j + 1] = temp;
         }
-        System.out.println("Array string sorted by Descending order " + Arrays.toString(digitals));
+        System.out.println("Array string sorted by Descending order " + Arrays.toString(digits));
 
         /*3. Вывести на консоль те числа, длина которых меньше (больше) средней,
         а также длину*/
-        showLessThanLengthAverage(digitals);
-        showMoreThanLengthAverage(digitals);
+        showLessThanLengthAverage(digits);
+        showMoreThanLengthAverage(digits);
 
         /*4. Найти число, в котором число различных цифр минимально. Если таких
          чисел несколько, найти первое из них.*/
+        minDifDigit(digits);
+
+        /*5. Найти количество чисел, содержащих только четные цифры, а среди них —
+        количество чисел с равным числом четных и нечетных цифр.*/
+        equalsEvenOddDigits(digits);
+
+        /*6. Найти число, цифры в котором идут в строгом порядке возрастания. Если
+        таких чисел несколько, найти первое из них.*/
+        arithmeticProgression(digits);
+
+        /*7. Найти число, состоящее только из различных цифр. Если таких чисел несколько
+        , найти первое из них*/
+        differentDigits(digits);
+
     }
 
     private static void showLessThanLengthAverage(String [] array){
@@ -130,5 +144,91 @@ public class Main {
             if(nums[i] == 1) count++;
         }
         return count;
+    }
+
+    private static void minDifDigit(String [] array){
+        String numberWithMinDifDigit = "";
+
+        int min = 11;
+        for(String st: array){
+            if(uniqueNum(st)<min){
+                min=uniqueNum(st);
+                numberWithMinDifDigit=st;
+            }
+        }
+        System.out.println("Number with minimal count of unique digit is " + numberWithMinDifDigit);
+    }
+
+    private static boolean isEvenOdd(String element){
+        int even = 0, odd = 0;
+        for(int i = 0; i < element.length(); i++){
+            if(Character.getNumericValue(element.charAt(i)) % 2 == 0)
+                even++;
+            else odd++;
+        }
+        if(even == odd)
+            return true;
+        else
+            return false;
+    }
+
+    private static void equalsEvenOddDigits(String [] array){
+        ArrayList<String> result = new ArrayList();
+        for(String iterator:array){
+            if(Integer.parseInt(iterator) % 2 == 0){
+                result.add(iterator);
+            }
+        }
+        int count=0;
+        System.out.println("Numbers with equal number of even and odd digits:");
+        for(String element: result){
+            if(isEvenOdd(element)){
+                System.out.print(element+" ");
+                count++;
+            }
+        }
+        System.out.println("\nCount of even numbers with equal number of even and odd digits is "+count);
+    }
+
+    private static boolean isProgression(String st){
+        for(int i = 0; i<st.length();i++) {
+            for (int j = i+1;j<st.length();j++){
+                if (Character.getNumericValue(st.charAt(i))>Character.getNumericValue(st.charAt(j)))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private static void arithmeticProgression(String [] array){
+        ArrayList<String> result = new ArrayList();
+        String arithmeticProgressionNumber;
+        for(String iterator : array){
+            if (Integer.parseInt(iterator) > 9)
+                result.add(iterator);
+        }
+        for(String element: result){
+            if(isProgression(element)){
+                arithmeticProgressionNumber = element;
+                System.out.println("Number with digit in arithmetic progression " + arithmeticProgressionNumber);
+                break;
+            }
+        }
+    }
+
+    public static void differentDigits(String [] array){
+        ArrayList<String> result = new ArrayList();
+        String numberWithUniqueDigitals;
+        for(String iterator : array){
+            if(iterator.length() < 11 && iterator.length() > 1){
+                result.add(iterator);
+            }
+        }
+        for(String st: result)
+            if (isUnique(st)) {
+                numberWithUniqueDigitals = st;
+                System.out.println("Number with unique digits is " + numberWithUniqueDigitals);
+                break;
+            }
     }
 }
